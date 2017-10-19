46a8caccdf0949a908f15e35a24dd33507b09d04
Carlo <carlo.namoca@gmail.com>
2017-10-19 10:56:48 -0700

    Initial Commit

---
+++ Wavy Collection View/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wavy Collection View
+//
+//  Created by Carlo Namoca on 2017-10-19.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wavy Collection ViewTests/Wavy_Collection_ViewTests.m
@@ -0,0 +1,39 @@
+//
+//  Wavy_Collection_ViewTests.m
+//  Wavy Collection ViewTests
+//
+//  Created by Carlo Namoca on 2017-10-19.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Wavy_Collection_ViewTests : XCTestCase
+
+@end
+
+@implementation Wavy_Collection_ViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
